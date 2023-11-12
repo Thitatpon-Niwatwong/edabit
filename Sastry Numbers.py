391111917551eae8adbd66e6ed25b5e7903dd281
Pee <64011150@kmitl.ac.th>
2023-11-12 22:47:58 +0700

    e

---
+++ Sastry Numbers.py
@@ -0,0 +1,11 @@
+def is_sastry(n):
+    from math import sqrt
+    if sqrt(n) % 1 == 0:
+        print(sqrt(n))
+        return True
+    else:
+        print(sqrt(n))
+        return False
+
+
+print(is_sastry(5))
